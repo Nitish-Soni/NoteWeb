8ff7a98b5e601ccc2113921768b0881656a87a9f
Nitish Soni <nikku@Nitishs-MacBook-Air.local>
2025-01-28 00:13:14 +0530

    Added Environment Variables

--- src/Database/Firebase.jsx
+++ src/Database/Firebase.jsx
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get, child, remove } from "firebase/database";
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyABv12r_kILa_L5lu_ZQh_sa_GIvOML8bM",
-  authDomain: "nitish-noteweb.firebaseapp.com",
-  projectId: "nitish-noteweb",
-  databaseURL:
-    "https://nitish-noteweb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: "nitish-noteweb.firebasestorage.app",
-  messagingSenderId: "938735945485",
-  appId: "1:938735945485:web:a62a4c02d86cbdaac4d244",
-  measurementId: "G-CCNQL0V2C4",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const FirebaseApp = initializeApp(FirebaseConfig);
